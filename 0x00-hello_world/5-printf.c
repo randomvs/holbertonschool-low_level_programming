9568abc06daec3253ad89bc77dd02f90dc0285b9
Victor Arellano <victor.arellano@holbertonschool.com>
2016-10-14 05:30:06 +0000

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
+ */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return(0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }

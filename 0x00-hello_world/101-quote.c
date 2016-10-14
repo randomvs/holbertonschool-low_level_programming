1f069eebafc65f1fae5a696543e25c23fd9d272c
Victor Arellano <victor.arellano@holbertonschool.com>
2016-10-14 19:56:19 +0000

    question 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Prints quote without using puts or printf
+ * main - Prints quote to the standard error
  *
  * Return: zero on success
  *
  */
 int main(void)
 {
-	char *s = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
-
-	while (*s)
-	{
-		putchar(*s++);
-	}
-	return (0);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }

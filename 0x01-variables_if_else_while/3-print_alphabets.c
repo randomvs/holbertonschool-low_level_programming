540b2a7929d93403b994a6d72ec1d68cbf929ec4
Victor Arellano <victor.arellano@holbertonschool.com>
2016-10-14 06:26:43 +0000

    question 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Prints alphabet
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints the alphabet in lower and upper case
+ * Can only use putchar
+ *
+ * Return: 0 for success
+*/
+int main(void)
+{
+	char letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	letter = 'A';
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}

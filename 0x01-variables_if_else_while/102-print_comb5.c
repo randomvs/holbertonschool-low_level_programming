092becbbccb40af65ed60c93cf62a695fcece4d2
Victor Arellano <victor.arellano@holbertonschool.com>
2016-10-18 03:55:26 +0000

    revise comma

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ int main(void)
 	{
 		for (j = 0; j <= 99; j++)
 		{
-			if (i < j && i != j)
+			if (i < j)
 			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
@@ -25,7 +25,7 @@ int main(void)
 				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
 
-				if (i != 99 || j != 99)
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');

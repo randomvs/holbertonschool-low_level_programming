b4bd0612d3e1b43fb60277e35a32f61c0f5437ae
Victor Arellano <victor.arellano@holbertonschool.com>
2016-10-19 06:45:59 +0000

    question 4

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -5,7 +5,7 @@
  * print_alphabet - prints alphabet and returns new line
  * print_alphabet_x10 - print alphabet 10 times
  * _islower - return 1 if c is lowercase, 0 otherwise
- *
+ * _isalpha - return 1 if c is upper or lowercase, 0 otherwise
  *
  *
  **/
@@ -13,3 +13,4 @@ int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);

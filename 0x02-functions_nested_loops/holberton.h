a71dee5b7f5c6ed1e8bca0b2582c0561eee641e6
Victor Arellano <victor.arellano@holbertonschool.com>
2016-10-19 06:09:05 +0000

    question 2

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -4,8 +4,10 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- *print_alphabet prints alphabet and returns new line
+ * print_alphabet - prints alphabet and returns new line
+ * print_alphabet_x10 - print alphabet 10 times
  *
- */
+ **/
 int _putchar(char);
 void print_alphabet(void);
+void print_alphabet_x10(void);

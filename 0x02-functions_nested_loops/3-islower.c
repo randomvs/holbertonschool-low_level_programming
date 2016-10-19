92bfcfd417bebd1b7c2a5c5f54b441d186b827e5
Victor Arellano <victor.arellano@holbertonschool.com>
2016-10-19 06:27:16 +0000

    question 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+
+/**
+ * print_alphabet - prints lower case alphabet
+ *
+ **/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,12 +2,14 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
- *
  * print_alphabet - prints alphabet and returns new line
  * print_alphabet_x10 - print alphabet 10 times
+ * _islower - return 1 if c is lowercase, 0 otherwise
+ *
+ *
  *
  **/
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);

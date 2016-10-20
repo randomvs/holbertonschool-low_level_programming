8fd0cb5298ad2dffb54329fe7bc2e31737198577
Victor Arellano <victor.arellano@holbertonschool.com>
2016-10-20 21:06:43 +0000

    question 4

--- 0x03-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,7 +11,10 @@ void print_most_numbers(void)
 	char i;
 
 	for (i = '0'; i <= '9'; i++)
-		if ((i!= 2) && (i!= 4))
-			_putchar(i);
+	{
+		if ((i == '2') || (i == '4'))
+			i++;
+		_putchar(i);
+	}
 	_putchar('\n');
 }

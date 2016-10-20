bec818bc56abb0b098e5fdd546fa83df475e046f
Victor Arellano <victor.arellano@holbertonschool.com>
2016-10-20 03:24:55 +0000

    question 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - print the first 50 fibonacci numbers
+ *
+ * Return: Nothing.
+ */
+
+int main(void)
+{
+	int count;
+	unsigned long x, y, z;
+
+	x = 0;
+	y = 1;
+
+	for (count = 1; count <= 49; count++)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+		printf("%lu, ", z);
+	}
+
+	printf("%lu\n", z);
+	return (0);
+}

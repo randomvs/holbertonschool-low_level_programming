0933161f7290a2e3ae18b65b778b6fbb3e295b1b
Victor Arellano <victor.arellano@holbertonschool.com>
2016-10-20 03:20:53 +0000

    question 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - print sum of all even fibonacci numbers under 4x10^6
+ *
+ * Return: Nothing.
+ */
+
+int main(void)
+{
+	unsigned long x, y, z, sum;
+
+	z = 0;
+	x = 0;
+	y = 1;
+	sum = 0;
+
+	while (z < 4000000)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+
+		if (z % 2 == 0)
+			sum += z;
+	}
+
+	printf("%lu\n", sum);
+	return (0);
+}

1782ad70f3e49f27eb2a74131c4c36273fa1a2c3
Victor Arellano <victor.arellano@holbertonschool.com>
2016-10-20 21:09:17 +0000

    question 5

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 
 /**
- *
  * more_numbers - print 0-14 10 times
  * Return: nothing
  **/
@@ -9,6 +8,7 @@
 void more_numbers(void)
 {
 	int i, j;
+
 	for (j = 0; j <= 9; j++)
 	{
 		for (i = 0; i <= 14; i++)

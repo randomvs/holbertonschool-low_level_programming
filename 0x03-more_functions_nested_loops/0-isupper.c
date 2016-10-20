fd06dc0aca65ac40f845a8974405d42e9135877b
Victor Arellano <victor.arellano@holbertonschool.com>
2016-10-20 16:16:52 +0000

    question 0

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+
+/**
+ * _isupper - checks if value is upper case
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if c is capital, otherwise 0
+ **/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}

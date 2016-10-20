c6f03ffee6b0ac9f77d375001183f498871ca869
Victor Arellano <victor.arellano@holbertonschool.com>
2016-10-20 22:46:29 +0000

    FizzBuzz

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Fizz-Buzz test
+ *
+ * Description: print numbers 1 -100 replacing multiples of 3 with Fizz,
+ * multiples of 5 with Buzz, and multiples of both with FizzBuzz
+ *
+ * Return: zero
+ **/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	putchar('\n');
+	return (0);
+}

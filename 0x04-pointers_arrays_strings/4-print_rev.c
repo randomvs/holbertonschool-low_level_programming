e4830146a3b8f78360c9f44e3ae8fd3b596f6eb3
randomvs <victorearellano@gmail.com>
2016-10-25 17:23:55 +0000

    question 4

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,13 @@
 
 void print_rev(char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        while (s[i] != '\0')
-                i++;
+	while (s[i] != '\0')
+		i++;
 
-	for (; i > 0; i--)
+	for (; i >= 0; i--)
 		_putchar(s[i]);
+
 	_putchar('\n');
 }

96be6d5d8e44194b4e1c819dc420d9b60ad2ba03
randomvs <victorearellano@gmail.com>
2016-10-25 16:44:11 +0000

    question 0

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,11 @@
 /**
  * reset_to_98 - set value of int to 98
  * @n: address of value to change
- *
+ * Description: resets *n to 98
  * Return: nothing
  **/
 
 void reset_to_98(int *n)
 {
-  *n = 98;
+	*n = 98;
 }

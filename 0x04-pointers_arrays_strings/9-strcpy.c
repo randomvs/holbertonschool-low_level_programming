9a34aa2e5a8bdc5a47059e5eadd027bac7961b6d
randomvs <victorearellano@gmail.com>
2016-10-26 04:12:39 +0000

    question 9

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -14,11 +14,12 @@ char *_strcpy(char *dest, char *src)
 
 	i = 0;
 
-	for (i = 0; src[i] != '\0', i++)
+	while (i >= 0)
 	{
 		dest[i] = src[i];
-	}
+		i++;
 
-	dest[i] = '\0';
-	return (dest);
+		if (src[i] == '\0')
+			return dest;
+	}
 }

fc4ba80a7299f43384b0899b29588fe4e41029a4
randomvs <victorearellano@gmail.com>
2016-10-28 02:46:23 +0000

    question 3

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 
 	i = 0;
 
-	while (s1[i] != '\0' && s2[i] != 0)
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] > s2[i])
 			return (15);
@@ -27,7 +27,7 @@ int _strcmp(char *s1, char *s2)
 	if (s1[i] == s2[i])
 		return (0);
 	else if (s1[i] == '\0')
-		return (-15);
+		return (15);
 
-	return (15);
+	return (-15);
 }

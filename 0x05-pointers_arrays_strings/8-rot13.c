07dd3825640f29c4bfb3c05bd41163f5b160f53b
randomvs <victorearellano@gmail.com>
2016-10-28 02:34:03 +0000

    question 8

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -8,14 +8,14 @@
 
 char *rot13(char *src)
 {
-        char c[] = {"NOPQRSTUVWXYZABCDEFGHIJKLM[\\]^_`nopqrstuvwxyzabcdefghijklm"};
+	char c[] = {"NOPQRSTUVWXYZABCDEFGHIJKLM[\\]^_`nopqrstuvwxyzabcdefghijklm"};
 
-        int i;
+	int i;
 
-        for (i = 0; src[i] != '\0'; i++)
-        {
-                if (src[i] >= 'A' && src[i] <= 'z')
-                        src[i] = c[src[i] - 'A'];
-        }
-        return (src);
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		if (src[i] >= 'A' && src[i] <= 'z')
+			src[i] = c[src[i] - 'A'];
+	}
+	return (src);
 }

1e5a9061398d5cfe0ff9246989d7cbc68abf5141
randomvs <victorearellano@gmail.com>
2016-10-31 20:56:24 +0000

    question 5

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+
+/**
+ * compare - compares two strings
+ * @haystack: source string
+ * @needle: string being compared
+ * Description: checks if needle is at start of haystack
+ * Return: 1 if matches, 0 if not
+ **/
+
+int compare(char *haystack, char *needle)
+{
+	int i;
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		if (haystack[i] == '\0')
+			return (0);
+		if (haystack[i] != needle[i])
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * _strstr - locates a substring
+ * @haystack: source string
+ * @needle: substring
+ * Description: finds first instance of needle in haystack
+ * Return: pointer to first instance, NULL if not in haystack
+ **/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (needle[i] == haystack[j])
+			{
+				if (compare(&haystack[j], needle) == 1)
+					return (&haystack[j]);
+			}
+		}
+	}
+	return ('\0');
+}

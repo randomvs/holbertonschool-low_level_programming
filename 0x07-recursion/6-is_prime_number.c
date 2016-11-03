2a99be8df6b3bfef1d7064770640f18e266564bc
randomvs <victorearellano@gmail.com>
2016-11-03 00:09:25 +0000

    question 6

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * prime_con - determine if n is a prime
+ * @n: test case
+ * @i: recurring factors
+ * Description: determines if n is a prime using 2 parameters
+ * Return: 1 if n is a prime, 0 if not
+ **/
+
+int prime_con(int n, int i)
+{
+	if (n % i == 0)
+		return (0);
+
+	else if (i + 1 < n / 2 + 1)
+		return (prime_con(n, i + 1));
+	return (1);
+}
+
+/**
+ * is_prime_number - determine if n is a prime
+ * @n: number to check
+ * Description: determine if n is a prime
+ * Return: 1 if prime, 0 if not
+ **/
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+
+	return (prime_con(n, 2));
+}

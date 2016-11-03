eef3f43faa395667c4483b243a16d67b51c7dde5
randomvs <victorearellano@gmail.com>
2016-11-03 16:43:59 +0000

    question 4 revised

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -14,10 +14,5 @@ int _pow_recursion(int x, int y)
 	if (y == 0)
 		return (1);
 
-	if (y > 1)
-	{
-		x = x * _pow_recursion(x, y - 1);
-	}
-
-	return (x);
+	return (x * _pow_recursion(x, y - 1));
 }

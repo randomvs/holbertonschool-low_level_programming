1522823995909ea1b4da606b1acfdbba4a8360eb
randomvs <victorearellano@gmail.com>
2016-11-07 01:55:17 +0000

    question 4

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - find least amount of coins needed for change
- * @argc: number of parameters entered
- * @argv: strings entered including file name
- * Description: prints the minimum number of coins to make change
- * Return: zero
+ * numchecker - verifies if string has only numbers
+ * @s: string to check
+ * Description: return 1 if only numbers in string, 0 if not
+ * Return: 1 if only numbers, 0 if not
  **/
 
 int numchecker(char *s)
 {
 	int i;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] < '0' || s[i] > '9')
@@ -19,6 +20,14 @@ int numchecker(char *s)
 	}
 	return (1);
 }
+
+/**
+ * main - find least amount of coins needed for change
+ * @argc: number of parameters entered
+ * @argv: strings entered including file name
+ * Description: prints the minimum number of coins to make change
+ * Return: zero
+ **/
 
 int main(int argc, char **argv)
 {

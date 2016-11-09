725b88633d432246b617899efa5ec27a8a1df536
randomvs <victorearellano@gmail.com>
2016-11-09 21:27:52 +0000

    q2 s1/s2 empty

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -38,8 +38,9 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL && s2 == NULL)
 	{
 		dest = "\0";
-		return (dest);
+		return (NULL);
 	}
+
 	dest = (char *)malloc((i + j - 1) * sizeof(char));
 
 	if (dest == NULL)
@@ -47,9 +48,10 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0'; i++)
 		dest[i] = s1[i];
+
 	for (j = 0; s2[j] != '\0'; j++)
 		dest[i + j] = s2[j];
 
-	dest[i + j] = '\0';
+	dest[i + j] = s2[j];
 	return (dest);
 }

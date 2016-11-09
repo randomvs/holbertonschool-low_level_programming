812581831c9bff55466c92f03464b8985ffe5810
randomvs <victorearellano@gmail.com>
2016-11-09 17:35:26 +0000

    q0 add size checker

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -11,8 +11,13 @@
  **/
 char *create_array(unsigned int size, char c)
 {
-	char *s = (char *)malloc(size * sizeof(char));
+	char *s;
 	unsigned int i;
+
+	if (size <= 0)
+		return (NULL);
+
+	s = (char *)malloc(size * sizeof(char));
 
 	if (s == NULL)
 		return (NULL);

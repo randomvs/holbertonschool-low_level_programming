77ca8025fe8d251b36d1d5ae9e62082f0bab73ed
randomvs <victorearellano@gmail.com>
2016-11-10 21:47:51 +0000

    question 1

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -2,15 +2,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * create_array -
- * @size: size of array
- * @c: char to assign
- * Description:
- * Return:
+ * _strlen - returns the length of the string
+ * @s: input string to count
+ * Description: returns the length of a given string
+ * Return: length of string as int
+ **/
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	if (s == NULL)
+		return (1);
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i + 1);
+}
+/**
+ * string_nconcat - concatenate n chars of second string to first
+ * @s1: first string
+ * @s2: second string
+ * @n: number of chars to concatenate
+ * Description: concatenate two strings, return NULL if fails
+ * Return: pointer to string, NULL if fails
  **/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *dest;
+	unsigned int size1, size2, i, j;
 
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+	if (n > size2 - 1)
+		n = size2 - 1;
 
-	return (0);
+	dest = (char *)malloc((size1 + n) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+
+	for (i = 0; i < size1 - 1; i++)
+		dest[i] = s1[i];
+
+	for (j = 0; j <= n; j++)
+	{
+		dest[i + j] = s2[j];
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
 }
--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -103,3 +103,8 @@ char **strtow(char *str);
 int word_count(char *s, int word);
 int wordlen(char *s);
 int findword(char *s);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);

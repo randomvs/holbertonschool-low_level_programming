55e5b079ab9fb7297700382af54270c80d03b510
randomvs <victorearellano@gmail.com>
2016-11-15 21:04:20 +0000

    q0 double inclusion correction

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,7 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#ifndef DOGGY
-#define DOGGY
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
  * struct dog - dog object
  * @name: name of dog
@@ -15,4 +13,5 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif

c40d6d9c84df68e195b502194b2089e0f82d3b67
randomvs <victorearellano@gmail.com>
2016-11-16 23:54:58 +0000

    question 2

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - search for int using cmp
+ * @array: array to operate on
+ * @size: size of array
+ * @cmp: compare function
+ * Return: nothing
+ **/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}

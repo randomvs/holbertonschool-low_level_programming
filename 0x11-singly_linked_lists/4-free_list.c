27ccbdd9f57bb6a64685c6e8692a9dab19c34a4c
randomvs <victorearellano@gmail.com>
2016-11-29 02:34:57 +0000

    q4

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -10,14 +10,14 @@
  **/
 void free_list(list_t *head)
 {
-	list *next = head->next;
+	list_t *next;
 
 	while (head != NULL)
 	{
 		if (head->str != NULL)
 			free(head->str);
+		next = head->next;
 		free(head);
 		head = next;
-		next = head->next;
 	}
 }

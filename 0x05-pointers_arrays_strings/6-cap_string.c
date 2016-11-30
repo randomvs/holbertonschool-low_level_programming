860013302e1a4d1a1b97a46df0f6ce1b823632fe
randomvs <victorearellano@gmail.com>
2016-11-30 01:13:10 +0000

    corrected q6

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
+#include <stddef.h>
+#include <stdio.h>
 
 /**
- * string_toupper - changes all lower case to capital letters
- * @str: string to modify
- * Description: changes all lower case letters to capitals
- * Return: char
+ * upper - capitalizes a letter if it is lowercase
+ * @c: pointer to a string
+ * Return: pointer to string
  **/
-
 char *upper(char *c)
 {
 	if (c[0] >= 'a' && c[0] <= 'z')
@@ -14,6 +14,11 @@ char *upper(char *c)
 	return (c);
 }
 
+/**
+ * new_word - compares char to chars that indicate a new word
+ * @c: char to check
+ * Return: 1 if char is a word separator, 0 of not
+ **/
 int new_word(char c)
 {
 	int i = 0;
@@ -23,21 +28,33 @@ int new_word(char c)
 	{
 		if (c == word[i])
 			return (1);
+		i++;
 	}
 
 	return (0);
 }
 
+/**
+ * cap_string - capitalize all words in a string
+ * @str: string to modify
+ * Return: pointer to str
+ **/
 char *cap_string(char *str)
 {
 	int i;
 
-	upper(*str);
+	if (str == NULL)
+		return (NULL);
+	upper(str);
+	if (str[0] == '\0')
+		return (str);
 
 	for (i = 1; str[i] != '\0'; i++)
 	{
 		if (new_word(str[i - 1]) == 1)
-			upper(*str[i]);
+		{
+			upper(&str[i]);
+		}
 	}
 	return (str);
 }

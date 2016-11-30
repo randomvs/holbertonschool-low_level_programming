fcc15915e5c19b225b1ecac9cb4988ba9c18f193
randomvs <victorearellano@gmail.com>
2016-11-30 00:34:27 +0000

    correct old code

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -17,16 +17,12 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] > s2[i])
-			return (15);
-		if (s1[i] < s2[i])
-			return (-15);
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
 
 	if (s1[i] == s2[i])
 		return (0);
-	else if (s1[i] > s2[i])
-		return (15);
-	return (-15);
+	return (s1[i] - s2[i]);
 }

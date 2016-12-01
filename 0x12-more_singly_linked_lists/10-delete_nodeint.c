00d068fbc78d6dfff25fbecdc356d9f34e9a9387
randomvs <victorearellano@gmail.com>
2016-12-01 19:18:41 +0000

    added addition null case for empty link

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *prior, *next;
 	unsigned int i;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	next = *head;

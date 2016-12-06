76f59f81268211a1a48866dcf169070db07181ac
randomvs <victorearellano@gmail.com>
2016-12-06 04:56:42 +0000

    q3

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -3,11 +3,27 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ * binary_index - returns 2 ^ y
+ * @y: exponent value
+ * Return: 2 ^ y or 0 if fail
+ */
+int binary_index(int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (2);
+	return (2 * binary_index(y - 1));
+}
+
+/**
+ * set_bit - changes a bit at index to 1
+ * @n: pointer to int
+ * @index: index to change
+ * Return: 1 if successful, -1 if failed
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	return (*n + index);
+	*n = *n | binary_index(index);
+	return (1);
 }
--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -7,6 +7,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int power(char, int y);
 int _putchar(char c);
 #endif

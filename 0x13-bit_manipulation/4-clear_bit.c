a6953edfbb4cc1dc34af1910a4d97b1d9eb60495
randomvs <victorearellano@gmail.com>
2016-12-10 20:28:22 +0000

    q4

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -2,12 +2,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 /**
- *
- *
- *
+ * clear_bit - set bit and index to 0
+ * @n: pointer to int
+ * @index: index to change
+ * Return: 1 if successful, -1 if failed
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	return (index + *n);
+	unsigned long int i;
+
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+
+	i = *n ^ 1 << index;
+	*n = *n & i;
+	return (1);
 }

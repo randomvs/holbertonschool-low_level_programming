3fa155bee8a53c27d7c6730cc0ab35c4b9b9e224
randomvs <victorearellano@gmail.com>
2016-12-10 19:53:34 +0000

    q2 add limit with sizeof

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > sizeof(unsigned long int))
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
 
 	n = n >> index;

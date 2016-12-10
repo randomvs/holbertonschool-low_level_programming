a879e7e1f3b18cdd4ac4a93d445fa521e18a12b6
randomvs <victorearellano@gmail.com>
2016-12-10 20:41:42 +0000

    q5

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -3,11 +3,24 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ * flip_bits - counts the number of bits needed to flip to go from n to m
+ * @n: first unsigned long int
+ * @m: second unsigned long int
+ * Return: number of bits flipped
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	return (m + n);
+	unsigned int i;
+
+	i = 0;
+	n = n ^ m;
+
+	while (n > 0)
+	{
+		if (n & 1)
+			i++;
+		n = n >> 1;
+	}
+
+	return (i);
 }

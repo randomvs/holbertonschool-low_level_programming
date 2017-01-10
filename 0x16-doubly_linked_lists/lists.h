350353b197cce71cbc9c37eedbadc950870c56fa
randomvs <victorearellano@gmail.com>
2017-01-10 03:20:17 +0000

    new lib

--- 0x16-doubly_linked_lists/lists.h
+++ 0x16-doubly_linked_lists/lists.h
@@ -3,7 +3,6 @@
 #include <stddef.h>
 #include <stddef.h>
 #include <stdlib.h>
-
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -27,5 +26,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *, unsigned int);
 int sum_dlistint(dlistint_t *head);
-
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 #endif

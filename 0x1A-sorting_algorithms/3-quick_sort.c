ef549d246f70b9065a715322b7403238080121d7
randomvs <victorearellano@gmail.com>
2017-02-27 04:01:14 +0000

    q3 files

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -1,41 +1,14 @@
 #include "sort.h"
 
 /**
- * swap - swap values of two indices in an array
- * @array: int array
- * @indexA: index of first value
- * @indexB: index of second value
- * Return: nothing
+ *quick_sort - implement quicksort on an array of ints
+ *@array: array of ints
+ *@size: size of array
  */
-void swap(int *array, size_t indexA, size_t indexB)
-{
-	int temp;
 
-	temp = array[indexA];
-	array[indexA] = array[indexB];
-	array[indexB] = temp;
-}
-
-/**
- * quick_sort - use quicksort on an array
- * @array: array of int's
- * @size: size of array
- * Return: nothing
- */
 void quick_sort(int *array, size_t size)
 {
- 	size_t index, runner, pivot, wall;
-
-	if (array == NULL)
-		return;
-
-	index = 0;
-	wall = 0;
-	runner = 0;
-
-	if (array[index] > array[pivot])
-	{
-		swap(array, index, pivot);
-
-	}
+	array = array;
+	size = size;
+	printf("still hashing algo\n");
 }
